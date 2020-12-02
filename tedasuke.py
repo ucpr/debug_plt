6fcde653eaaf93e872cde9253be44d226b0b0f8e
ucpr <contact@ucpr.dev>
2020-12-02 16:50:31 +0900

    chore: add comment

--- tedasuke.py
+++ tedasuke.py
@@ -8,6 +8,7 @@ class tedasuke:
         self._level = logger.getEffectiveLevel()
 
     def __getattr__(self, name):
+        # https://github.com/python/cpython/blob/master/Lib/logging/handlers.py#L722
         if self._passing_level < self._level:
             return self._cb
         return getattr(self._instance, name)

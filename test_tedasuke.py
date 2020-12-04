5f0d6aa3230c5344d0621f8d321b0c85951d2709
ucpr <contact@ucpr.dev>
2020-12-05 02:32:55 +0900

    test: fix test

--- test_tedasuke.py
+++ test_tedasuke.py
@@ -1,24 +1,13 @@
-from logging import getLogger, DEBUG, INFO
-import pytest
-
 from tedasuke import tedasuke
 
 
-@pytest.fixture
-def logger():
-    l = getLogger(__name__)
-    return l
-
-
-def test_tedasuke(logger):
-    logger.setLevel(DEBUG)
-    t = tedasuke("str ing", logger, level=DEBUG)
-    result = t.split()
+def test_tedasuke():
+    t = tedasuke("str ing", True)
     want = ["str", "ing"]
-    assert result == want
-
-    logger.setLevel(INFO)
-    t = tedasuke("str ing", logger, level=DEBUG)
     result = t.split()
+    assert want == result
+
+    t = tedasuke("str ing", False)
     want = None
-    assert result == want
+    result = t.split()
+    assert want == result
